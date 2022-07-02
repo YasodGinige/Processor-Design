be986bd66045355f1a581d03db3d6c0ab4df6639
Ransara <ransarawijitha@gmail.com>
2022-07-02 15:29:10 +0530

    Decoder C operation added.  Changes to be committed: 	modified:   hdl/Decoder.v

--- hdl/Decoder.v
+++ hdl/Decoder.v
@@ -21,20 +21,39 @@
 
 
 module Decoder(
-    input [3:0]in,
+    input [3:0]sel,
     input EN_OP,
     input EN_OUT,
-    output reg OUT [11:0]
+    output wire OUT [11:0],
+    output str_pointer,
+    output mar,
+    output mdr,
+    output pr1,
+    output pr2,
+    output pr3,
+    output col,
+    output row,
+    output r1,
+    output r2
     );
     
     reg [11:0]temp;
-    always@(in)
+    assign temp[0] = str_pointer;
+    assign temp[3] = mar;
+    assign temp[4] = mdr;
+    assign temp[5] = pr1;
+    assign temp[6] = pr2;
+    assign temp[7] = pr3;
+    assign temp[8] = col;
+    assign temp[9] = row;
+    assign temp[10] = r1;
+    assign temp[11] = r2;
+    
+    always@(sel)
     begin
     if (EN_OP==1)begin
     case (in)
       4'b0001: temp=12'b000000000001;
-      4'b0010: temp=12'b000000000010;
-      4'b0011: temp=12'b000000000100;
       4'b0100: temp=12'b000000001000;
       4'b0101: temp=12'b000000010000;
       4'b0110: temp=12'b000000100000;
@@ -49,5 +68,6 @@ module Decoder(
     if (EN_OUT==1)begin
         OUT = temp;
     end
+    
     end
 endmodule
